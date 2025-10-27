35f823a0cfda38597f463f9efbb9987f7df3f776
Anish Ghimire <pnoob189@gmail.com>
2025-10-27 21:57:49 +0545

    tiny changes

--- server/src/config/ingest.js
+++ server/src/config/ingest.js
@@ -6,7 +6,7 @@ export const inngest = new Inngest({ id: "stack-clone" });
 
 const syncUser = inngest.createFunction(
     { id: "sync-user" },
-    { event: "clerk/user.created" },
+    { event: "user.created" },
     async ({ event }) => {
         const database = db;
         const { id, email_addresses, first_name, image_url, created_at, updated_at } = event.data;
@@ -31,7 +31,7 @@ const syncUser = inngest.createFunction(
 
 const deleteUser = inngest.createFunction(
     { id: "delete-user" },
-    { event: "clerk/user.deleted" },
+    { event: "user.deleted" },
     async ({ event }) => {
         const database = db;
         const { id } = event.data;
